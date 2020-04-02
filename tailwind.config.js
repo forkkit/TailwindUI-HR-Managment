ce6a91109bbcb1dca300a2100373f9bc5e6efb33
JKH <jan-kevin.heise@travelbasys.de>
2020-04-02 18:58:26 +0200

    Added Ecommerce

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '6xs': '.15rem',
+        '5xs': '.25rem',
+        '4xs': '.35rem',
+        '3xs': '.45rem',
+        '2xs': '.5rem',
+      }
+    },
   },
   variants: {},
   plugins: [
